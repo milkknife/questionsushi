f332f93a105b27a1456362e0bd4beffe308ee43c
milkknife <111794344+milkknife@users.noreply.github.com>
2023-02-08 13:58:36 +0100

    fix db, repeated outputs

--- questionsushi.py
+++ questionsushi.py
@@ -4,29 +4,25 @@
 '''
 from os import environ
 from sys import stdout
-from random import choice
+from random import choices
 from logging import StreamHandler
 from discord import Intents
 from discord.ext import commands
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 intents = Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='?', intents=intents)
 
-# sample questions, switch this with db file
-qdb = ('what is a class decorator?',
-       'what does `yield` do?')
-
-@bot.command(name='say', description='says something')
-async def ping(ctx, abc): # sample command
-    try:
-        await ctx.send(abc)
-        0/0 # experimenting with exception handling
-    except: # todo: handle invalid input
-        await ctx.send('nuh uh uh')
+with open('db.yml') as f:
+    qdb = load(f, Loader=Loader)
 
 @bot.command(name='question') # todo: add more parameters, send message like chron
-async def getq(ctx, lang): # todo: wait one minute, then announce the answer
-    await ctx.send(f'{lang}: {choice(qdb)}') # todo: state language
+async def getq(ctx): # todo: wait one minute, then announce the answer
+    await ctx.send(choices(qdb)[0]) # todo: state language
 
 bot.run(environ['DISCORD_BOT_TOKEN'], log_handler=StreamHandler(stdout))
