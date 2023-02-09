729ea78245cb1f69ad7776ad10fa8026a3f3eec9
milkknife <111794344+milkknife@users.noreply.github.com>
2023-02-10 00:47:49 +0100

    add task loop, ping

--- questionsushi.py
+++ questionsushi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-''' read -s DISCORD_BOT_TOKEN && export DISCORD_BOT_TOKEN
+''' read QS_CHANNEL && export QS_CHANNEL
+    read -s QS_TOKEN && export QS_TOKEN
     pypy3 questionsushi.py
 '''
 from os import environ
@@ -7,7 +8,7 @@ from sys import stdout
 from random import choices
 from logging import StreamHandler
 from discord import Intents
-from discord.ext import commands
+from discord.ext import commands, tasks
 from yaml import load
 try:
     from yaml import CLoader as Loader
@@ -21,8 +22,18 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 with open('db.yml') as f:
     qdb = load(f, Loader=Loader)
 
-@bot.command(name='question') # todo: add more parameters, send message like chron
-async def getq(ctx): # todo: wait one minute, then announce the answer
-    await ctx.send(choices(qdb)[0]) # todo: state language
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'response: {round(bot.latency, 1)}ms')
 
-bot.run(environ['DISCORD_BOT_TOKEN'], log_handler=StreamHandler(stdout))
+@bot.event
+async def on_ready():
+    AskQuestion.start()
+
+@tasks.loop(seconds=30)
+async def AskQuestion():
+    channel = bot.get_channel(environ['QS_CHANNEL'])
+    await channel.send(choices(qdb)[0])
+
+# todo: add choosable answers
+bot.run(environ['QS_TOKEN'], log_handler=StreamHandler(stdout))
